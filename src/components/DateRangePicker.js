7d5ad2531fee798b9fdb016b9270877e70bfc164
George_Shevtsov <george@keywee.co>
2019-06-12 14:23:12 +0300

    add onStaticRangeChange prop to DefinedRange

--- src/components/DateRangePicker.js
+++ src/components/DateRangePicker.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import DateRange from './DateRange';
-import DefinedRange from './DefinedRange';
-import { findNextRangeIndex, generateStyles } from '../utils.js';
-import classnames from 'classnames';
-import coreStyles from '../styles';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import DateRange from "./DateRange";
+import DefinedRange from "./DefinedRange";
+import { findNextRangeIndex, generateStyles } from "../utils.js";
+import classnames from "classnames";
+import coreStyles from "../styles";
 
 class DateRangePicker extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      focusedRange: [findNextRangeIndex(props.ranges), 0],
+      focusedRange: [findNextRangeIndex(props.ranges), 0]
     };
     this.styles = generateStyles([coreStyles, props.classNames]);
   }
   render() {
     const { focusedRange } = this.state;
     return (
-      <div className={classnames(this.styles.dateRangePickerWrapper, this.props.className)}>
+      <div
+        className={classnames(
+          this.styles.dateRangePickerWrapper,
+          this.props.className
+        )}
+      >
         <DefinedRange
           focusedRange={focusedRange}
           onPreviewChange={value => this.dateRange.updatePreview(value)}
@@ -42,7 +47,7 @@ DateRangePicker.defaultProps = {};
 DateRangePicker.propTypes = {
   ...DateRange.propTypes,
   ...DefinedRange.propTypes,
-  className: PropTypes.string,
+  className: PropTypes.string
 };
 
 export default DateRangePicker;
